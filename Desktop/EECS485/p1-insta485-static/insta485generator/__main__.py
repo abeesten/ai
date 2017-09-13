1ce4f2603952fe0ba83fa74c1c3f3d6a9b6d055a
Abe Estenson <abeesten@umich.edu>
2017-09-12 23:11:23 -0400

    read in json workin on making directories now

--- Desktop/EECS485/p1-insta485-static/insta485generator/__main__.py
+++ Desktop/EECS485/p1-insta485-static/insta485generator/__main__.py
@@ -1,9 +1,42 @@
 """Build static HTML site from directory of HTML templates and plain files."""
 
+import click
+import jinja2
+import json
+import os
+import errno
 
-def main():
-    """Top level command line interface."""
-    print("Hello world!")
+
+#Reads JSON file and puts it somewhere(haven't decided)
+def readJSON(input_dir):
+    with open(input_dir + '/config.json', 'r') as myfile:
+        jsonObject=json.load(myfile)
+    return jsonObject
+      
+
+    
+
+#Does the writing into a new file.
+def newFiles(jsonObject, input_dir):    #make list of urls
+    urls = [i['url'] for i in jsonObject]
+    for url in urls:
+        outPath = (input_dir + '/html' + url + 'index.html')
+        #if not os.path.exists(os.path.dirname(outPath)):
+         #   os.makedirs(os.path.dirname(input_dir + '/html'))
+
+        with open(outPath, 'w+') as outputFile:
+            print('hello', file=outputFile, end="")
+        #print(outPath)
+
+
+@click.command()
+@click.option('-v','-verbose', is_flag=True, help='Print more output.(still under construction)')
+@click.argument('INPUT_DIR', type=click.Path())
+def main(verbose, input_dir):
+    """Templated static website generator."""
+    json = readJSON(input_dir)
+    newFiles(json, input_dir)
+    
 
 
 if __name__ == "__main__":
